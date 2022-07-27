970c753362028d7f4b9285de4a80c331f9e52dfe
Saba-Var <vartasashvili94@gmail.com>
2022-07-27 21:03:42 +0400

    chore: generate mongodb connection url

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,18 @@
+import { connectToMongo } from 'config'
 import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
 
 const server = express()
+server.use(cors())
 
-server.listen(4444, () => {
-  console.log('server listening on port http://localhost:4444')
+dotenv.config()
+connectToMongo()
+
+server.use(express.json())
+
+server.listen(process.env.SERVER_PORT, () => {
+  console.log(
+    `server listening on port http://localhost:${process.env.SERVER_PORT}`
+  )
 })
