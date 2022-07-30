0affd1aa738ea8dd1a3995105ff0aaefeaf5e867
Saba-Var <vartasashvili94@gmail.com>
2022-07-30 09:48:42 +0400

    docs: add documenation for registration route

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -61,9 +61,9 @@ export const registerUser = async (
         false,
         async (err: any) => {
           if (err) {
-            return res
-              .status(400)
-              .json({ message: 'User registration failed!' })
+            return res.status(500).json({
+              message: 'User registration failed! Email could not be sent.',
+            })
           }
 
           const hashedPassword = await bcrypt.hash(password, 12)
@@ -75,9 +75,9 @@ export const registerUser = async (
           })
         }
       )
+    } else {
+      return res.status(401).json({ message: 'Sendgrid api key is missing!' })
     }
-
-    return res.status(401).json({ message: 'Sendgrid api key is missing!' })
   } catch (error: any) {
     return res.status(500).json({ message: error.message })
   }
